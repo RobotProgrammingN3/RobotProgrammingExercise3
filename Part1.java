ece56ed34fbb8e3d2709c14b5aa95e0a686beec8
franko0294 <francis.berti@hotmail.co.uk>
2014-02-12 11:11:37 +0000

    Update Part1.java
    Split part 1 into part1 and part1ii

--- Part1.java
+++ Part1.java
@@ -16,9 +16,6 @@ public class Part1 {
 	private final static int  min_dist = 1;
 	private final static DifferentialPilot pilot = new DifferentialPilot(1.13, 5.20, Motor.A, Motor.B, false);
 	private final static double max_speed = pilot.getMaxTravelSpeed();
-	private boolean m_suppressed;
-	private static int lightThreshold = 43;
-	private static int steerValue = 10000;
 	/**
 	 * @param args
 	 */
@@ -26,68 +23,7 @@ public class Part1 {
 		// TODO Auto-generated method stub
 			Button.waitForAnyPress();
 			runDistance();
-			//runLightSensor();
 			
-	}
-
-	private static void runLightSensor() {
-		// TODO Auto-generated method stub
-		LightSensor m_lightSensorR = new LightSensor(SensorPort.S1, true);
-		LightSensor m_lightSensorL = new LightSensor(SensorPort.S2, true);
-		m_lightSensorR.setFloodlight(Color.RED);
-		m_lightSensorL.setFloodlight(Color.RED);
-		//m_lightSensorR.setFloodlight(true);
-		//m_lightSensorL.setFloodlight(true);
-		//pilot.setTravelSpeed(((double) distance / (double) max_dist)*max_speed);
-		pilot.setTravelSpeed(pilot.getMaxTravelSpeed()/4);
-		
-		while(true){
-			System.out.println(m_lightSensorR.getLightValue());
-			pilot.forward();
-			Thread.yield();
-			makeDecision(m_lightSensorL.getLightValue(), m_lightSensorR.getLightValue());
-		}
-		//over brown sensor = 47
-		//on black sensor = 45
-	}
-	
-	private static void makeDecision(int lightValueL, int lightValueR) {
-		// TODO Auto-generated method stub
-		if(lightValueL < lightThreshold && lightValueR < lightThreshold)
-		{
-			//pilot.steer(20000);
-			pilot.forward();
-			Delay.msDelay(500);
-			pilot.rotate(90);
-		}
-		else if(lightValueL < lightThreshold)
-		{
-			//turn right
-			
-			//Double turnRate = calcTurnRate(lightValueL);
-			pilot.steer(-steerValue);
-			Thread.yield();
-		}
-		else if(lightValueR < lightThreshold)
-		{
-			// turn right
-			
-			//Double turnRate = calcTurnRate(lightValueR);
-			pilot.steer(steerValue);
-			Thread.yield();
-		}
-		
-	}
-
-	public void goForward()
-	{
-		pilot.forward();
-		while(!m_suppressed)
-		{
-			Thread.yield();
-		}
-		pilot.stop();
-		m_suppressed = false;
 	}
 
 	private static void runDistance() {
